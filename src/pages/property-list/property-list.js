5309a115bdbfa808b0928fc0afebebfacaa11269
patrifdz <fdzpatri@gmail.com>
2024-12-08 13:25:36 +0100

    feat: create imports to get functions from property-list.api and from property-list.mapper

--- src/pages/property-list/property-list.js
+++ src/pages/property-list/property-list.js
@@ -1,13 +1,3 @@
-console.log('works')
+import { getPropertyList } from './property-list.api';
+import { mapPropertyListFromApiToViewModel } from './property-list.mapper';
 
-/*
-Property {
-    id: string,
-    title: string,
-    rooms: string, // 3 habitaciones
-    squareMeter: string, // 30 m2
-    notes: string, // "truncar" -> a 240 carácteres
-    price: string, // 120.000 €
-    image: string // 1ª image base64
-    }
-*/
